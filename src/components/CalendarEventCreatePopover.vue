68e4e8b0ea3ad658c8d87e81f7ccc8f551ee0842
priede <daisypriede@gmail.com>
2018-12-09 09:29:10 -0500

    new popover

--- src/components/CalendarEventCreatePopover.vue
+++ src/components/CalendarEventCreatePopover.vue
@@ -192,6 +192,24 @@
          </v-list-tile-content>
        </v-list-tile>
 
+        <v-list-tile v-if="prompts.busy && $dayspan.supports.busy">
+         <v-list-tile-avatar>
+           <v-icon>work</v-icon>
+         </v-list-tile-avatar>
+         <v-list-tile-content>
+           <slot name="eventCreatePopoverBusy" v-bind="slotData">
+
+             <v-select
+              single-line hide-details solo flat full-width
+              :items="newOptions"
+              v-model="details.busy"
+            ></v-select>
+
+           </slot>
+         </v-list-tile-content>
+       </v-list-tile>
+
+
      </v-list>
 
      <slot name="eventCreatePopoverBodyBottom" v-bind="slotData"></slot>
@@ -282,7 +300,7 @@ export default {
     {
       type: Array,
       default() {
-        return this.$dsDefaults().busyOptions;
+        return this.$dsDefaults().newOptions;
       }
     }
   },
